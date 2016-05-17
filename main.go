a9d0ae49ec61f52eaecec41609c48fb8a8e94af6
Travis Scholl <tscholl2@uw.edu>
2016-05-16 23:22:16 -0700

    one pattern to htink about

--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"math/rand"
 	"net/http"
+	"text/template"
 )
 
 //go:generate embd -n basicSVG static/basic.1.svg
 
+// <path id="lip" d="M50,50 a 30,60 30 0,0 30,0"/>
+type lip struct {
+	Angle int    // 30
+	Width int    // 30
+	Style string // ""
+}
+
+type values struct {
+	Lip string
+}
+
 var (
-	tmpl *template.Template
+	tmpl = template.Must(template.New("basic").Parse(basicSVG))
 )
 
-func init() {
-	tmpl = template.Must(template.New("basic").Parse(basicSVG))
+func newHam() (v values) {
+	// lip
+	angle := rand.Intn(360) - 180
+	width := rand.Intn(20) + 15
+	frown := rand.Intn(2)
+	style := ""
+	v.Lip = `<path id = "lip"`
+	if style != "" {
+		v.Lip += ` style="` + style + `"`
+	}
+	var y int
+	if frown > 0 {
+		y = 55
+	} else {
+		y = 50
+	}
+	v.Lip += fmt.Sprintf(` d="M%d,%d a %d,60 %d 0,%d %d,0"/>`, 65-width/2, y, width, angle, frown, width)
+	return v
 }
 
 func get(w http.ResponseWriter, r *http.Request) {
-	tmpl.Execute(w, struct{ Lip int }{Lip: rand.Intn(270) - 90})
+	tmpl.Execute(w, newHam())
 }
 
 func headers(h http.Handler) http.Handler {
